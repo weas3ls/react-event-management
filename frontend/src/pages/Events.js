e4da2f170925b3a9b7ceeac2b5a1f3cc9d82c941
weas3ls <shabaneh.ahmad@gmail.com>
2024-10-26 16:17:29 +0200

    implement error handling

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,9 +1,13 @@
-import { useLoaderData } from "react-router-dom";
+import { json, useLoaderData } from "react-router-dom";
 
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
-    const events = useLoaderData();
+    const data = useLoaderData();
+
+    if (data.isError) return <p>{data.message}</p>;
+
+    const events = data.events;
 
     return <EventsList events={events} />;
 }
@@ -14,9 +18,11 @@ export async function loader() {
     const response = await fetch("http://localhost:8080/events");
 
     if (!response.ok) {
-        //...
+        // return { isError: true, message: "Could not fetch events" };
+        // throw new Response(JSON.stringify({ message: "Could not fetch events" }), { status: 500 });
+
+        return json({ message: "Could not fetch events" }, { status: 500 });
     } else {
-        const resData = await response.json();
-        return resData.events;
+        return response;
     }
 }

0172ac775dd46d92ca9504315195536b64579169
weas3ls <shabaneh.ahmad@gmail.com>
2024-11-10 18:47:11 -0800

    implement event deletion

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,4 +1,4 @@
-import { json, useRouteLoaderData } from "react-router-dom";
+import { json, redirect, useRouteLoaderData } from "react-router-dom";
 
 import EventItem from "../components/EventItem";
 
@@ -17,4 +17,14 @@ export async function loader({ req, params }) {
 
     if (!response.ok) throw json({ message: "Could not fetch details for selected event." }, { status: 500 });
     return response;
+}
+
+export async function action({ request, params }) {
+    const eventId = params.eventId;
+
+    const response = fetch("http://localhost:8080/events/" + eventId, { method: request.method });
+
+    if (!response.ok) throw json({ message: "Could not delete event." }, { status: 500 });
+
+    return redirect("/events");
 }

f0fb0966deb7cc1efa8aaed687bf4f9d343e073b
weas3ls <shabaneh.ahmad@gmail.com>
2024-11-10 18:59:48 -0800

    create generic action for editing and creating new events

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import { action as manipulateEventAction } from "./components/EventForm";
 import EditEventPage from "./pages/EditEvent";
 import ErrorPage from "./pages/Error";
 import EventDetailPage, { action as deleteEventAction, loader as eventDetailLoader } from "./pages/EventDetail";
 import EventsPage, { loader as eventsLoader } from "./pages/Events";
 import EventsRootLayout from "./pages/EventsRoot";
 import HomePage from "./pages/Home";
-import NewEventPage, { action as newEventAction } from "./pages/NewEvent";
+import NewEventPage from "./pages/NewEvent";
 import RootLayout from "./pages/Root";
 
 const router = createBrowserRouter([
@@ -41,13 +42,14 @@ const router = createBrowserRouter([
                             {
                                 path: "edit",
                                 element: <EditEventPage />,
+                                action: manipulateEventAction,
                             },
                         ],
                     },
                     {
                         path: "new",
                         element: <NewEventPage />,
-                        action: newEventAction,
+                        action: manipulateEventAction,
                     },
                 ],
             },
